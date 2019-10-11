783791496b54d4ea377ab70664f48c0d38841b2f
Dom <dominicsmith1021@gmail.com>
2019-10-10 23:45:45 -0400

    Required path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path');
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
